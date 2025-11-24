50416460be1381d86d06416ec85d2c5af010ba06
Alexis Pairetti <alexis.pairetti@kloutit.com>
2025-11-24 11:32:58 +0100

    feat(categories): Change categories + little fixes

--- base.ts
+++ base.ts
@@ -19,7 +19,7 @@ import type { Configuration } from './configuration';
 import type { AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = "http://localhost".replace(/\/+$/, "");
+export const BASE_PATH = "https://clients-api.kloutit.com".replace(/\/+$/, "");
 
 /**
  *
